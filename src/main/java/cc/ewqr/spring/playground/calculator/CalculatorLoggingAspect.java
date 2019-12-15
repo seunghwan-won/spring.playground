d42e0fd251bd8d84f483ab0c836adb567bd12e53
seunghwan-won <tmdghks24@naver.com>
2019-12-16 00:14:50 +0900

    update aop example

--- src/main/java/cc/ewqr/spring/playground/calculator/CalculatorLoggingAspect.java
+++ src/main/java/cc/ewqr/spring/playground/calculator/CalculatorLoggingAspect.java
@@ -1,10 +1,13 @@
 package cc.ewqr.spring.playground.calculator;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
@@ -12,8 +15,38 @@ public class CalculatorLoggingAspect {
     private static final Logger logger = LoggerFactory.getLogger(CalculatorLoggingAspect.class);
 
     @Before("execution(* ArithmeticCalculator.add(..))")
-    public int beforeLog() {
-        logger.debug("The method add() begins");
-        return 2;
+    public void beforeLog() {
+        logger.info("The method add() begins");
     }
+
+    @Before("execution(* *.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        logger.info("The method " + joinPoint.getSignature().getName()
+                + "() begins with " + Arrays.toString(joinPoint.getArgs()));
+    }
+
+    @After("execution(* *.*(..))")
+    public void logAfter(JoinPoint joinPoint) {
+        logger.info("The method " + joinPoint.getSignature().getName() + "() ends");
+    }
+
+    @AfterReturning(pointcut = "execution(* *.*(..))", returning = "result")
+    public void logAfterReturning(JoinPoint joinPoint, Object result) {
+        logger.info("The method {}() ends with {}", joinPoint.getSignature().getName(), result);
+    }
+
+    @Around("execution(* *.*(..))")
+    public Object logAround(ProceedingJoinPoint pjp) throws Throwable {
+        logger.info("The method {}() begins with {}", pjp.getSignature().getName(), Arrays.toString(pjp.getArgs()));
+
+        try {
+            Object result = pjp.proceed();
+            logger.info("The method {}() ends with ", pjp.getSignature().getName(), result);
+            return result;
+        } catch (IllegalArgumentException e ) {
+            logger.error("Illegal argument {} in {}()", Arrays.toString(pjp.getArgs()), pjp.getSignature().getName());
+            throw e;
+        }
+    }
+
 }
--- src/test/java/cc/ewqr/spring/playground/aop/AOPTest.java
+++ src/test/java/cc/ewqr/spring/playground/aop/AOPTest.java
@@ -2,12 +2,9 @@ package cc.ewqr.spring.playground.aop;
 
 import cc.ewqr.spring.playground.calculator.ArithmeticCalculator;
 import cc.ewqr.spring.playground.calculator.CalculatorConfiguration;
-import cc.ewqr.spring.playground.calculator.CalculatorLoggingAspect;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import static org.junit.Assert.assertEquals;
 
 public class AOPTest {
     @Test
@@ -15,7 +12,5 @@ public class AOPTest {
         ApplicationContext context = new AnnotationConfigApplicationContext(CalculatorConfiguration.class);
         ArithmeticCalculator calculator = context.getBean("arithmeticCalculator", ArithmeticCalculator.class);
         calculator.add(1, 1);
-        CalculatorLoggingAspect calculatorLoggingAspect = new CalculatorLoggingAspect();
-        assertEquals(2,calculatorLoggingAspect.beforeLog());
     }
 }
